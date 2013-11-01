d151d09d8ee33054486e17a0ad7c9d4213cfc2fa
Brandon Burnett <gentoolicious@gmail.com>
2013-11-01 15:44:16 -0700

    Added redis to __init__.py

--- scanflask/__init__.py
+++ scanflask/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask.ext.script import Manager
+import redis
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 manager = Manager(app)
+
+store = redis.StrictRedis(host='localhost', port=6378, db=0)
 
 from scanflask import views
